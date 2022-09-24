7440644a72cf4c3b94209476aed014d95ea25b2d
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-24 02:01:38 -0700

    Completed first challenge

---
+++ 1-buffer-from-string.js
@@ -0,0 +1,6 @@
+// const { Buffer } = require('node:buffer');
+// const buff = Buffer.from('bytewiser', 'utf-8');
+// console.log(buff);
+
+// official solution
+console.log(new Buffer('bytewiser'));
